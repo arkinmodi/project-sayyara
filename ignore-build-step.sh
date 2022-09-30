1680ed7654dd3b26815596c867d46d03c23fdb18
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2022-09-30 13:40:24 -0400

    Create build ignore script (#79)
    * Create build ignore script
    
    Close https://github.com/arkinmodi/project-sayyara/issues/78
    
    * update url
    
    * words

---
+++ ignore-build-step.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# exit 1 = build, exit 0 = skip build
+# https://vercel.com/docs/concepts/projects/overview#ignored-build-step
+
+if [[ -z $(git diff-tree --no-commit-id --name-only HEAD | grep "^src*") ]]; then
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0;
+else
+  # Proceed with the build
+  echo "✅ - Build can proceed"
+  exit 1;
+fi
