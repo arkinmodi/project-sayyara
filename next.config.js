cfe22ab6b9aa206a1dbdaf9e00043113f8517c98
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2022-10-07 13:20:39 -0400

    Configure Nextjs App for PWA Support
    Close https://github.com/arkinmodi/project-sayyara/issues/59

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
