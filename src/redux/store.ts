1bfcddd749523dc5ccf8510e0a13ecdca7ba8c54
Leon So <Leon_so7@hotmail.com>
2022-10-15 21:08:25 -0400

    Plumbing for redux and sagas

---
+++ src/redux/store.ts
@@ -0,0 +1,30 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { Store } from "redux";
+import { createWrapper, Context } from "next-redux-wrapper";
+import createSagaMiddleware, { Task } from "redux-saga";
+import { rootSaga } from "./sagas";
+import { rootReducer } from "./reducers";
+
+export interface State {}
+
+export interface SagaStore extends Store {
+  sagaTask?: Task;
+}
+
+export const makeStore = (context: Context) => {
+  const sagaMiddleware = createSagaMiddleware();
+
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => [
+      ...getDefaultMiddleware(),
+      sagaMiddleware,
+    ],
+  });
+
+  (store as SagaStore).sagaTask = sagaMiddleware.run(rootSaga);
+
+  return store;
+};
+
+export const wrapper = createWrapper<Store<State>>(makeStore, { debug: false });
