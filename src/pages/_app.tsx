5934dba3804e338ebb2ba8c38d7e69176c72232c
Leon So <34189743+LeonSo7@users.noreply.github.com>
2022-10-15 21:33:11 -0400

    [Redux] Add plumbing for redux and sagas (#143)
    * Plumbing for redux and sagas
    
    * Update src/redux/store.ts
    
    Co-authored-by: Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
    
    Co-authored-by: Arkin Modi <16737086+arkinmodi@users.noreply.github.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
+import React, { FC } from "react";
+import { Provider } from "react-redux";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { wrapper } from "../redux/store";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const MyApp: FC<AppProps> = ({ Component, ...rest }) => {
+  const { store, props } = wrapper.useWrappedStore(rest);
+  return (
+    <Provider store={store}>
+      <Component {...props.pageProps} />
+    </Provider>
+  );
+};
 
 export default MyApp;
