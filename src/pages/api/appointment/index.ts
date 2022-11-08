98134b6b4434aa14c30dc24daa08a8e39308c9be
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2022-11-07 20:07:56 -0500

    add appointment controllers and service

---
+++ src/pages/api/appointment/index.ts
@@ -0,0 +1,33 @@
+import { getServerAuthSession } from "@server/common/getServerAuthSession";
+import {
+  createAppointment,
+  createAppointmentSchema,
+} from "@server/services/appointmentService";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const appointmentHandler = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  const user = await getServerAuthSession({ req, res });
+  if (!user) {
+    res.status(403).json({ message: "Forbidden." });
+    return;
+  }
+
+  if (req.method !== "POST") {
+    res.status(405).json({ message: "Method not allowed." });
+    return;
+  }
+
+  const result = createAppointmentSchema.safeParse(req.body);
+  if (!result.success) {
+    res.status(400).json({ message: result.error.issues });
+    return;
+  }
+
+  const newAppointment = await createAppointment(result.data);
+  res.status(201).json(newAppointment);
+};
+
+export default appointmentHandler;
---
+++ src/server/services/appointmentService.ts
@@ -0,0 +1,150 @@
+import { AppointmentStatus, prisma, ServiceType } from "@server/db/client";
+import { z } from "zod";
+
+export const createAppointmentSchema = z.object({
+  quote_id: z.string().optional(),
+  vehicle_id: z.string(),
+  service_type: z.nativeEnum(ServiceType),
+  price: z.number().min(0).optional(),
+  employee_id: z.string().optional(),
+  customer_id: z.string(),
+  start_time: z.date(),
+  end_time: z.date(),
+  shop_id: z.string(),
+});
+export type CreateAppointmentType = z.infer<typeof createAppointmentSchema>;
+
+export const createAppointment = async (appointment: CreateAppointmentType) => {
+  const now = new Date();
+
+  if (
+    appointment.start_time > appointment.end_time ||
+    appointment.start_time < now ||
+    appointment.end_time < now
+  ) {
+    return Promise.reject("Invalid start time and/or end time.");
+  }
+
+  return await prisma.appointment.create({
+    data: {
+      status: "PENDING_APPROVAL",
+      start_time: appointment.start_time,
+      end_time: appointment.end_time,
+      price: appointment.price,
+      service_type: appointment.service_type,
+      vehicle: {
+        connect: { id: appointment.vehicle_id },
+      },
+      work_order: {
+        create: { create_time: now, update_time: now },
+      },
+      customer: {
+        connect: { id: appointment.customer_id },
+      },
+      quote: {
+        connect: { id: appointment.quote_id },
+      },
+      employee: {
+        connect: { id: appointment.employee_id },
+      },
+      shop: {
+        connect: { id: appointment.shop_id },
+      },
+    },
+  });
+};
+
+export const getAppointmentById = async (id: string) => {
+  return await prisma.appointment.findUnique({ where: { id } });
+};
+
+export const updateAppointmentSchema = z.object({
+  quote_id: z.string().optional(),
+  work_order_id: z.string().optional(),
+  vehicle_id: z.string().optional(),
+  service_type: z.nativeEnum(ServiceType).optional(),
+  price: z.number().min(0).optional(),
+  employee_id: z.string().optional(),
+  customer_id: z.string().optional(),
+  status: z.nativeEnum(AppointmentStatus).optional(),
+  start_time: z.date().optional(),
+  end_time: z.date().optional(),
+});
+export type UpdateAppointmentType = z.infer<typeof updateAppointmentSchema>;
+
+export const updateAppointmentById = async (
+  id: string,
+  patch: UpdateAppointmentType
+) => {
+  const appointment = await prisma.appointment.findUnique({ where: { id } });
+  if (!appointment) return Promise.reject("Appointment not found.");
+
+  const now = new Date();
+  if (
+    (patch.start_time && patch.start_time < now) ||
+    (patch.end_time && patch.end_time < now) ||
+    (patch.start_time && patch.end_time && patch.start_time > patch.end_time)
+  ) {
+    return Promise.reject("Invalid start time and/or end time.");
+  }
+
+  if (
+    appointment.status === "PENDING_APPROVAL" &&
+    patch.status === "ACCEPTED"
+  ) {
+    acceptAppointment(id);
+  }
+
+  return await prisma.appointment.update({ where: { id }, data: patch });
+};
+
+const acceptAppointment = async (id: string) => {
+  const acceptedAppointment = await prisma.appointment.findUnique({
+    where: { id },
+  });
+  if (!acceptedAppointment) return Promise.reject("Appointment not found.");
+
+  await prisma.appointment.update({
+    where: { id },
+    data: {
+      status: "ACCEPTED",
+    },
+  });
+
+  // Reject all other appointments conflicting
+  await prisma.appointment.updateMany({
+    where: {
+      AND: [
+        { start_time: { gte: acceptedAppointment.start_time } },
+        { end_time: { lte: acceptedAppointment.start_time } },
+      ],
+      NOT: [
+        { id: { equals: acceptedAppointment.id } },
+        { status: { equals: "REJECTED" } },
+      ],
+    },
+    data: {
+      status: "REJECTED",
+    },
+  });
+
+  await prisma.appointment.updateMany({
+    where: {
+      AND: [
+        { start_time: { gte: acceptedAppointment.end_time } },
+        { end_time: { lte: acceptedAppointment.end_time } },
+      ],
+      NOT: [
+        { id: { equals: acceptedAppointment.id } },
+        { status: { equals: "REJECTED" } },
+      ],
+    },
+    data: {
+      status: "REJECTED",
+    },
+  });
+};
+
+export const deleteAppointment = async (id: string) => {
+  return await prisma.appointment.delete({ where: { id } });
+};
