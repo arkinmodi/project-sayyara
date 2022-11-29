c8ae2b6aef7f22cda9fc1daada70a388648615c6
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2022-11-29 16:49:27 -0500

    update global css imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,14 @@
-import "@blueprintjs/core/lib/css/blueprint.css";
-import "@blueprintjs/icons/lib/css/blueprint-icons.css";
-import "@blueprintjs/datetime2/lib/css/blueprint-datetime2.css";
-import "@blueprintjs/datetime/lib/css/blueprint-datetime.css";
-import "@blueprintjs/popover2/lib/css/blueprint-popover2.css";
-import "@blueprintjs/select/lib/css/blueprint-select.css";
+import { wrapper } from "@redux/store";
 import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import { FC } from "react";
 import { Provider } from "react-redux";
-import { wrapper } from "../redux/store";
+
+import "primeicons/primeicons.css"; // icons
+import "primereact/resources/primereact.min.css"; // core css
+import "primereact/resources/themes/lara-light-indigo/theme.css"; // theme
+
 import "../styles/globals.css";
 
 const MyApp: FC<
