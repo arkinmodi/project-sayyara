371e119ca8bc7210d2099e518a32a68c9abefcc1
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2022-12-22 18:52:59 -0500

    [Header] Use Next/Image (#376)
    It is recommended by Next.js to use next/image for loading images. TLDR it is more performant.
    
    More detailed answers:
    - https://nextjs.org/docs/messages/no-img-element
    - https://nextjs.org/docs/basic-features/image-optimization
    - https://stackoverflow.com/a/67886607

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,13 +1,20 @@
 import styles from "@styles/components/common/Header.module.css";
+import Image from "next/image";
 import { Menubar } from "primereact/menubar";
 import { MenuItem } from "primereact/menuitem";
+import logo from "public/logo.png";
 import React from "react";
 
 const Header = () => {
   const items: MenuItem[] = [];
 
   const start = (
-    <img alt="logo" src="/logo.png" height="40" className="mr-2"></img>
+    <Image
+      src={logo}
+      alt="Sayyara Logo"
+      height={logo.height * 0.75}
+      width={logo.width * 0.75}
+    />
   );
   return <Menubar className={styles.menuBar} model={items} start={start} />;
 };
