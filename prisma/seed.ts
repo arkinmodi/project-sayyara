6c53b7463d7e03b1b550db0b91e8c5f5386bc356
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2022-12-30 11:44:14 -0500

    [Rev 0 Demo] Create Database Seed Script
    Close https://github.com/arkinmodi/project-sayyara/issues/380

---
+++ prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+const shopOwnerCredentials = {
+  email: "shopOwner@sayyara.com",
+  password: "password",
+};
+
+const customerCredentials = {
+  email: "customer@sayyara.com",
+  password: "password",
+};
+
+const clearDatabase = async () => {
+  await prisma.$transaction([
+    prisma.employee.deleteMany({}),
+    prisma.customer.deleteMany({}),
+    prisma.appointment.deleteMany({}),
+    prisma.quote.deleteMany({}),
+    prisma.vehicle.deleteMany({}),
+    prisma.workOrder.deleteMany({}),
+    prisma.shop.deleteMany({}),
+    prisma.service.deleteMany({}),
+  ]);
+};
+
+const seed = async () => {
+  await clearDatabase();
+
+  // Create Shop Owner
+  await prisma.employee.create({
+    data: {
+      email: shopOwnerCredentials.email,
+      password: shopOwnerCredentials.password,
+      first_name: "John",
+      last_name: "Stone",
+      type: "SHOP_OWNER",
+    },
+  });
+
+  // Create Customer
+  await prisma.customer.create({
+    data: {
+      email: customerCredentials.email,
+      password: customerCredentials.password,
+      first_name: "Mia",
+      last_name: "Wong",
+      type: "CUSTOMER",
+    },
+  });
+};
+
+seed()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
