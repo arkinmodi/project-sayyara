33d16a1a0e3e426ea27166fbf809cc3226bcbc80
Joy Xiao <34189744+joyxiao99@users.noreply.github.com>
2023-01-11 14:37:39 -0500

    update

---
+++ src/components/appointments/shop/appointmentsTabs.tsx
@@ -0,0 +1,26 @@
+import styles from "@styles/pages/appointments/Requests.module.css";
+import { TabPanel, TabView } from "primereact/tabview";
+import Requested from "./requested";
+
+const AppointmentsTab = () => {
+  return (
+    <div className={styles.appointmentRequestsContainer}>
+      <TabView>
+        <TabPanel header="Requested">
+          <Requested />
+        </TabPanel>
+        <TabPanel header="Scheduled">
+          {/* TODO: Link Scheduled tab here */}
+        </TabPanel>
+        <TabPanel header="In Progress">
+          {/* TODO: Link In Progress tab here */}
+        </TabPanel>
+        <TabPanel header="Completed">
+          {/* TODO: Link Completed tab here */}
+        </TabPanel>
+      </TabView>
+    </div>
+  );
+};
+
+export default AppointmentsTab;
--- src/components/appointments/shop/requested.tsx
+++ src/components/appointments/shop/requested.tsx
@@ -1,15 +1,13 @@
 import { AppointmentSelectors } from "@redux/selectors/appointmentSelectors";
 import AppointmentTypes from "@redux/types/appointmentTypes";
 import styles from "@styles/pages/appointments/Requests.module.css";
-import { NextPage } from "next";
-import { Button } from "primereact/button";
 import { Card } from "primereact/card";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppointmentStatus, IAppointment } from "../../../types/appointment";
+import AppointmentCard from "./appointmentCard";
 
-// TODO: pass in the appointments list from the db and order it according to the date/start-time. For each appointment with the same date, display the info in cards
-const OrderedAppointments: NextPage = () => {
+const Requested = () => {
   const [pendingAppointments, setPendingAppointments] = useState<
     IAppointment[]
   >([]);
@@ -36,11 +34,6 @@ const OrderedAppointments: NextPage = () => {
     setPendingAppointments(pendingAppointments);
   }, [appointments, setPendingAppointments]);
 
-  const handleAcceptButtonClick = (appointment: IAppointment): void => {
-    const payload = { id: appointment.id, status: AppointmentStatus.ACCEPTED };
-    dispatch({ type: AppointmentTypes.SET_APPOINTMENT_STATUS, payload });
-  };
-
   return (
     <div className={styles.appointmentRequestsContainer}>
       <Card className={styles.appointmentRequestsCard}>
@@ -49,22 +42,7 @@ const OrderedAppointments: NextPage = () => {
         </h2>
         {pendingAppointments.length > 0 ? (
           pendingAppointments.map((appointment) => (
-            <Card key={appointment.id.toString()}>
-              <div>Appointment id: {appointment.id}</div>
-              <div>
-                Start time:{" "}
-                {String(new Date(appointment.startTime).toLocaleString())}
-              </div>
-              <div>
-                End time:{" "}
-                {String(new Date(appointment.endTime).toLocaleString())}
-              </div>
-              <Button
-                label="Accept"
-                className={styles.appointmentRequestsAcceptButton}
-                onClick={() => handleAcceptButtonClick(appointment)}
-              />
-            </Card>
+            <AppointmentCard appointment={appointment} />
           ))
         ) : (
           <div className={styles.appointmentRequestsCardText}>
@@ -76,4 +54,4 @@ const OrderedAppointments: NextPage = () => {
   );
 };
 
-export default OrderedAppointments;
+export default Requested;
