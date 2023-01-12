29dcb6d0694729c7c200f09908f3550de9395bd4
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2023-01-12 17:26:00 -0500

    [Rev 0 Demo] Add Phone Numbers to Users (#405)
    * [Rev 0 Demo] Add Phone Numbers to Users
    
    Relate https://github.com/arkinmodi/project-sayyara/issues/291

--- test/pages/api/service/service.integration.test.ts
+++ test/pages/api/service/service.integration.test.ts
@@ -34,6 +34,7 @@ const testEmployeeUser: Employee = {
   id: "test_id",
   first_name: "first_name",
   last_name: "last_name",
+  phone_number: "1234567890",
   email: "user@test.com",
   password: "test_password",
   image: null,
