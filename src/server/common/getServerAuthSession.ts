db244faf48284c8a9b38695bde345e0ed3aa2ac5
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2023-01-30 16:28:19 -0500

    use new next-auth server side method

--- src/server/common/getServerAuthSession.ts
+++ src/server/common/getServerAuthSession.ts
@@ -2,11 +2,11 @@
 
 import { authOptions as nextAuthOptions } from "@pages/api/auth/[...nextauth]";
 import type { GetServerSidePropsContext } from "next";
-import { unstable_getServerSession } from "next-auth";
+import { getServerSession } from "next-auth";
 
 export const getServerAuthSession = async (ctx: {
   req: GetServerSidePropsContext["req"];
   res: GetServerSidePropsContext["res"];
 }) => {
-  return await unstable_getServerSession(ctx.req, ctx.res, nextAuthOptions);
+  return await getServerSession(ctx.req, ctx.res, nextAuthOptions);
 };
