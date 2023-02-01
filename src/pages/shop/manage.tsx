85966334f0769b070463d44819e2d6a62cc276ce
Joy Xiao <34189744+joyxiao99@users.noreply.github.com>
2023-01-31 22:47:11 -0500

    add manage shop tab

---
+++ src/pages/shop/manage.tsx
@@ -0,0 +1,17 @@
+import { TabPanel, TabView } from "primereact/tabview";
+import React from "react";
+
+const ManageShopTabs = () => {
+  return (
+    <TabView>
+      <TabPanel header="Services">
+        {/* //TODO: Add manage services page here */}
+      </TabPanel>
+      <TabPanel header="Employees">
+        {/* //TODO: Add manage employees page here */}
+      </TabPanel>
+    </TabView>
+  );
+};
+
+export default React.memo(ManageShopTabs);
