7457b91ad361d41a0ee7dad2d3edee6ec72ccfe5
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2023-03-06 14:50:54 -0500

    [VnV Report] Add Unit Tests for User Module (#590)
    * [VnV Report] Add Unit Tests for User Module
    
    Close https://github.com/arkinmodi/project-sayyara/issues/459
    
    * create unit test summary table

--- test/pages/api/user/register.integration.test.ts
+++ test/pages/api/user/register.integration.test.ts
@@ -94,143 +94,196 @@ afterAll(async () => {
 });
 
 afterEach(async () => {
-  const deleteEmployees = prisma.employee.deleteMany({});
-  const deleteCustomers = prisma.customer.deleteMany({});
-  await prisma.$transaction([deleteEmployees, deleteCustomers]);
+  await prisma.$transaction([
+    prisma.vehicle.deleteMany(),
+    prisma.employee.deleteMany(),
+    prisma.customer.deleteMany(),
+    prisma.shop.deleteMany(),
+  ]);
 });
 
-describe("new user registration", () => {
-  describe("given valid new user data", () => {
-    it("should create new customer", async () => {
-      const { req, res } = createMockRequestResponse({ method: "POST" });
-      req.body = {
-        email: testCustomer.email,
-        password: testCustomer.password,
-        first_name: testCustomer.first_name,
-        last_name: testCustomer.last_name,
-        phone_number: testCustomer.phone_number,
-        vehicle: {
+describe("User Module", () => {
+  it("FRT-M3-1: create customer request received with valid information", async () => {
+    const { req, res } = createMockRequestResponse({ method: "POST" });
+    req.body = {
+      email: testCustomer.email,
+      password: testCustomer.password,
+      first_name: testCustomer.first_name,
+      last_name: testCustomer.last_name,
+      phone_number: testCustomer.phone_number,
+      vehicle: {
+        year: testCustomer.vehicles[0]?.year,
+        make: testCustomer.vehicles[0]?.make,
+        model: testCustomer.vehicles[0]?.model,
+        vin: testCustomer.vehicles[0]?.vin,
+        license_plate: testCustomer.vehicles[0]?.license_plate,
+      },
+    };
+
+    await registerCustomerHandler(req, res);
+    const newCustomer = await prisma.customer.findUnique({
+      where: { email: testCustomer.email },
+      include: { vehicles: true },
+    });
+
+    expect(res.statusCode).toBe(302);
+    expect(newCustomer).toEqual({
+      id: expect.any(String),
+      first_name: testCustomer.first_name,
+      last_name: testCustomer.last_name,
+      phone_number: testCustomer.phone_number,
+      email: testCustomer.email,
+      password: expect.any(String),
+      image: null,
+      create_time: expect.any(Date),
+      update_time: expect.any(Date),
+      type: "CUSTOMER",
+      vehicles: [
+        {
+          id: expect.any(String),
+          create_time: expect.any(Date),
+          update_time: expect.any(Date),
           year: testCustomer.vehicles[0]?.year,
           make: testCustomer.vehicles[0]?.make,
           model: testCustomer.vehicles[0]?.model,
           vin: testCustomer.vehicles[0]?.vin,
           license_plate: testCustomer.vehicles[0]?.license_plate,
+          customer_id: expect.any(String),
         },
-      };
-
-      await registerCustomerHandler(req, res);
-      const newCustomer = await prisma.customer.findUnique({
-        where: { email: testCustomer.email },
-        include: { vehicles: true },
-      });
-
-      expect(res.statusCode).toBe(302);
-      expect(newCustomer).toEqual({
-        id: expect.any(String),
-        first_name: testCustomer.first_name,
-        last_name: testCustomer.last_name,
-        phone_number: testCustomer.phone_number,
-        email: testCustomer.email,
-        password: expect.any(String),
-        image: null,
-        create_time: expect.any(Date),
-        update_time: expect.any(Date),
-        type: "CUSTOMER",
-        vehicles: [
-          {
-            id: expect.any(String),
-            create_time: expect.any(Date),
-            update_time: expect.any(Date),
-            year: testCustomer.vehicles[0]?.year,
-            make: testCustomer.vehicles[0]?.make,
-            model: testCustomer.vehicles[0]?.model,
-            vin: testCustomer.vehicles[0]?.vin,
-            license_plate: testCustomer.vehicles[0]?.license_plate,
-            customer_id: expect.any(String),
-          },
-        ],
-      });
-    });
-
-    it("should create new employee", async () => {
-      // Create Shop
-      // TODO: Use REST controller
-      const shop = await prisma.shop.create({
-        data: {
-          phone_number: testShopOwner.shop.phone_number,
-          email: testShopOwner.shop.email,
-          name: testShopOwner.shop.name,
-          address: testShopOwner.shop.address,
-          postal_code: testShopOwner.shop.postal_code,
-          city: testShopOwner.shop.city,
-          province: testShopOwner.shop.province,
-        },
-      });
-      testEmployee.shop_id = shop.id;
-
-      const { req, res } = createMockRequestResponse({ method: "POST" });
-      req.body = {
-        email: testEmployee.email,
-        password: testEmployee.password,
-        first_name: testEmployee.first_name,
-        last_name: testEmployee.last_name,
-        phone_number: testEmployee.phone_number,
-        shop_id: testEmployee.shop_id,
-      };
-
-      await registerEmployeeHandler(req, res);
-      const newEmployee = await prisma.employee.findUnique({
-        where: { email: testEmployee.email },
-      });
-
-      expect(res.statusCode).toBe(302);
-      expect(newEmployee).toEqual({
-        id: expect.any(String),
-        create_time: expect.any(Date),
-        update_time: expect.any(Date),
-        first_name: testEmployee.first_name,
-        last_name: testEmployee.last_name,
-        phone_number: testEmployee.phone_number,
-        email: testEmployee.email,
-        password: expect.any(String),
-        image: null,
-        type: testEmployee.type,
-        shop_id: testEmployee.shop_id,
-        status: testEmployee.status,
-      });
-    });
-
-    it("should create new shop owner", async () => {
-      const { req, res } = createMockRequestResponse({ method: "POST" });
-      req.body = {
-        email: testShopOwner.email,
-        password: testShopOwner.password,
-        first_name: testShopOwner.first_name,
-        last_name: testShopOwner.last_name,
-        phone_number: testShopOwner.phone_number,
-        shop: testShopOwner.shop,
-      };
-
-      await registerShopOwnerHandler(req, res);
-      const newShopOwner = await prisma.employee.findUnique({
-        where: { email: testShopOwner.email },
-      });
-
-      expect(res.statusCode).toBe(302);
-      expect(newShopOwner).toEqual({
-        id: expect.any(String),
-        create_time: expect.any(Date),
-        update_time: expect.any(Date),
-        first_name: testShopOwner.first_name,
-        last_name: testShopOwner.last_name,
-        phone_number: testShopOwner.phone_number,
-        email: testShopOwner.email,
-        password: expect.any(String),
-        image: null,
-        type: testShopOwner.type,
-        shop_id: expect.any(String),
-        status: testEmployee.status,
-      });
+      ],
     });
   });
+
+  it("FRT-M3-2: create customer request received with invalid information", async () => {
+    const { req, res } = createMockRequestResponse({ method: "POST" });
+    req.body = {
+      email: testCustomer.email,
+      password: testCustomer.password,
+      first_name: testCustomer.first_name,
+      last_name: testCustomer.last_name,
+      phone_number: testCustomer.phone_number,
+    };
+
+    await registerCustomerHandler(req, res);
+
+    expect(res.statusCode).toBe(400);
+  });
+
+  it("FRT-M3-3: create shop owner request received with valid information", async () => {
+    const { req, res } = createMockRequestResponse({ method: "POST" });
+    req.body = {
+      email: testShopOwner.email,
+      password: testShopOwner.password,
+      first_name: testShopOwner.first_name,
+      last_name: testShopOwner.last_name,
+      phone_number: testShopOwner.phone_number,
+      shop: testShopOwner.shop,
+    };
+
+    await registerShopOwnerHandler(req, res);
+    const newShopOwner = await prisma.employee.findUnique({
+      where: { email: testShopOwner.email },
+    });
+
+    expect(res.statusCode).toBe(302);
+    expect(newShopOwner).toEqual({
+      id: expect.any(String),
+      create_time: expect.any(Date),
+      update_time: expect.any(Date),
+      first_name: testShopOwner.first_name,
+      last_name: testShopOwner.last_name,
+      phone_number: testShopOwner.phone_number,
+      email: testShopOwner.email,
+      password: expect.any(String),
+      image: null,
+      type: testShopOwner.type,
+      shop_id: expect.any(String),
+      status: testEmployee.status,
+    });
+  });
+
+  it("FRT-M3-4: create shop owner request received with invalid information", async () => {
+    const { req, res } = createMockRequestResponse({ method: "POST" });
+    req.body = {
+      email: testShopOwner.email,
+      password: testShopOwner.password,
+      first_name: testShopOwner.first_name,
+      last_name: testShopOwner.last_name,
+      phone_number: testShopOwner.phone_number,
+    };
+
+    await registerCustomerHandler(req, res);
+
+    expect(res.statusCode).toBe(400);
+  });
+
+  it("FRT-M3-5: create employee request received with valid information", async () => {
+    // Create Shop
+    const shop = await createShop();
+    testEmployee.shop_id = shop.id;
+
+    const { req, res } = createMockRequestResponse({ method: "POST" });
+    req.body = {
+      email: testEmployee.email,
+      password: testEmployee.password,
+      first_name: testEmployee.first_name,
+      last_name: testEmployee.last_name,
+      phone_number: testEmployee.phone_number,
+      shop_id: testEmployee.shop_id,
+    };
+
+    await registerEmployeeHandler(req, res);
+    const newEmployee = await prisma.employee.findUnique({
+      where: { email: testEmployee.email },
+    });
+
+    expect(res.statusCode).toBe(302);
+    expect(newEmployee).toEqual({
+      id: expect.any(String),
+      create_time: expect.any(Date),
+      update_time: expect.any(Date),
+      first_name: testEmployee.first_name,
+      last_name: testEmployee.last_name,
+      phone_number: testEmployee.phone_number,
+      email: testEmployee.email,
+      password: expect.any(String),
+      image: null,
+      type: testEmployee.type,
+      shop_id: testEmployee.shop_id,
+      status: testEmployee.status,
+    });
+  });
+
+  it("FRT-M3-6: create employee request received with invalid information", async () => {
+    // Create Shop
+    const shop = await createShop();
+    testEmployee.shop_id = shop.id;
+
+    const { req, res } = createMockRequestResponse({ method: "POST" });
+    req.body = {
+      email: testEmployee.email,
+      password: testEmployee.password,
+      first_name: testEmployee.first_name,
+      last_name: testEmployee.last_name,
+      shop_id: testEmployee.shop_id,
+    };
+
+    await registerEmployeeHandler(req, res);
+
+    expect(res.statusCode).toBe(400);
+  });
 });
+
+const createShop = async () => {
+  return await prisma.shop.create({
+    data: {
+      phone_number: testShopOwner.shop.phone_number,
+      email: testShopOwner.shop.email,
+      name: testShopOwner.shop.name,
+      address: testShopOwner.shop.address,
+      postal_code: testShopOwner.shop.postal_code,
+      city: testShopOwner.shop.city,
+      province: testShopOwner.shop.province,
+    },
+  });
+};
